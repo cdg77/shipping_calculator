8c261173bef022962613a4b1a1f1ee5c13d39ee4
Travis Johnston & Carlo De Gregorio <student@epicodus.com>
2015-08-10 13:53:23 -0700

    created a test that takes the dimensions of a parcel and will expect a value

--- spec/parcels_spec.rb
+++ spec/parcels_spec.rb
@@ -1,2 +1,12 @@
 require('rspec')
 require('parcels')
+
+describe ('Parcel') do
+  describe ('#volume') do
+    it('takes the dimensions of a parcel returns the volume') do
+      new_parcel = Parcel.new(2, 4, 5)
+      expect(new_parcel.volume()).to(eq(40))
+    end 
+  end
+
+end
